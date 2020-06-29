65ea51cf6bf8bcc1c75cae39846dad70618be885
abizerjafferjee <abizerjafferjee@gmail.com>
2020-06-29 18:34:42 -0400

    setting debug in config

--- waterfrontapp/waterfrontapp/settings.py
+++ waterfrontapp/waterfrontapp/settings.py
@@ -35,7 +35,7 @@ def get_secret(setting, secrets=secrets):
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = get_secret("DEBUG")
 
 ALLOWED_HOSTS = get_secret("ALLOWED_HOSTS")
 
